23f8d0199fee352707d049fc9ac15e28872cde19
user <robert.czechonski@poczta.onet.pl>
2023-03-15 14:57:52 +0100

    asfsdgs

---
+++ src/main/java/Hello.java
@@ -0,0 +1,26 @@
+public class Hello {
+    static int wiekDelikwenta;
+    public static void main(String[] args){
+        final int ROK = 2023;   // deklaracja stałej
+        var zarobki = 100.50;   // deklaracja zmiennych
+        double pensja = 10000;
+
+        boolean done22,undone33;
+        long populacja_ziemi;
+        pensja = 50000;
+        wiekDelikwenta = 40;
+        var x = 10;
+        var y = 2.5;
+        var z = 2;
+        double wynik = x * y/z + x;
+        double w = Math.sqrt(x);
+        System.out.println(w);
+
+        System.out.println("Hello world!");
+        System.out.println(pensja);
+        System.out.println(wiekDelikwenta);
+        System.out.println(zarobki);
+        System.out.println(wynik);
+
+    }
+}
